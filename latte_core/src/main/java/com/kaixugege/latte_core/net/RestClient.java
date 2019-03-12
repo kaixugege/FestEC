a6099cfbf32148485f1215a600bc05f4fd6d983b
kaixugege <604312074@qq.com>
2019-03-12 15:57:19 +0800

    完善 net 框架。

--- latte_core/src/main/java/com/kaixugege/latte_core/net/RestClient.java
+++ latte_core/src/main/java/com/kaixugege/latte_core/net/RestClient.java
@@ -4,10 +4,13 @@ import android.content.Context;
 import com.kaixugege.latte_core.net.callback.*;
 import com.kaixugege.latte_core.ui.LatteLoader;
 import com.kaixugege.latte_core.ui.LoaderStyle;
+import okhttp3.MediaType;
+import okhttp3.MultipartBody;
 import okhttp3.RequestBody;
 import retrofit2.Call;
 import retrofit2.Callback;
 
+import java.io.File;
 import java.util.Map;
 
 /**
@@ -33,7 +36,9 @@ public class RestClient {
     private final IError ERROR;
     private final IFailure FAILURE;
     private final RequestBody BODY;
+    private final File FILE;
     private LoaderStyle LOADER_STYLE;
+
     private Context CONTEXT;
 
 
@@ -43,7 +48,10 @@ public class RestClient {
                       ISuccess SUCCESS,
                       IError ERROR,
                       IFailure FAILURE,
-                      RequestBody BODY, Context context, LoaderStyle loaderStyle) {
+                      RequestBody BODY,
+                      File file,
+                      Context context,
+                      LoaderStyle loaderStyle) {
         this.URL = mUrl;
         this.PARAMS.putAll(params);
         this.REQUEST = REQUEST;
@@ -53,6 +61,7 @@ public class RestClient {
         this.BODY = BODY;
         this.CONTEXT = context;
         this.LOADER_STYLE = loaderStyle;
+        this.FILE = file;
     }
 
     // 建造者，以Builder 方式传递
@@ -72,20 +81,42 @@ public class RestClient {
         }
 
         switch (method) {
+
             case GET:
                 call = service.get(URL, PARAMS);
                 break;
+
             case PUT:
                 call = service.put(URL, PARAMS);
                 break;
+
+            case PUT_RAW:
+                call = service.put(URL, PARAMS);
+                break;
+
             case POST:
                 call = service.post(URL, PARAMS);
                 break;
+
+            case POST_RAW:
+                call = service.postRaw(URL, BODY);
+                break;
+
             case DELETE:
                 call = service.delete(URL, PARAMS);
                 break;
+
+            case UPLOAD://上传
+                final RequestBody requestBody =
+                        RequestBody.create(MediaType.parse(MultipartBody.FORM.toString()), FILE);
+                final MultipartBody.Part body =
+                        MultipartBody.Part.createFormData("file", FILE.getName(),requestBody);
+                call = RestCreator.getRestService().upload(URL,body);
+                break;
+
             default:
                 break;
+
         }
         if (call != null) {
             call.enqueue(getRequestCallback());
@@ -97,20 +128,34 @@ public class RestClient {
         return new RequestCallbacks(REQUEST, SUCCESS, ERROR, FAILURE, LOADER_STYLE);
     }
 
-    //
-    //
-    //
+
     // 具体使用方法
     public final void get() {
         request(HttpMethod.GET);
     }
 
     public final void post() {
-        request(HttpMethod.POST);
+        if (BODY != null) {
+            request(HttpMethod.POST);
+        } else {
+            if (!PARAMS.isEmpty()) {
+                throw new RuntimeException("params must be null");
+            }
+            request(HttpMethod.POST_RAW);
+        }
+
     }
 
     public final void put() {
-        request(HttpMethod.PUT);
+
+        if (BODY != null) {
+            request(HttpMethod.PUT);
+        } else {
+            if (!PARAMS.isEmpty()) {
+                throw new RuntimeException("params must be null");
+            }
+            request(HttpMethod.PUT_RAW);
+        }
     }
 
     public final void delete() {
